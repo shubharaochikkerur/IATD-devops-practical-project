8384190655851e930d862e645adc67df1a0706f8
Shubha Rao-Chikkerur <shubha.raochikkerur3@tafensw.edu.au>
2025-10-07 14:31:05 +1100

    Added more code

--- lab1src.js
+++ lab1src.js
@@ -2,3 +2,4 @@ import readlineSync from 'readline-sync'
 console.log("Hello world, greetings from Shubha")
 const user = readlineSync.question("Who is using this program?")
 console.log("Welcome to my program, "+ user)
+console.log("another line of code")
