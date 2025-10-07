4c6746161e40e97583ca6bf3c8adef31156ce021
Shubha Rao-Chikkerur <shubha.raochikkerur3@tafensw.edu.au>
2025-10-07 14:15:57 +1100

    Added a file to the repo - my 1st attempt

---
+++ lab1src.js
@@ -0,0 +1,4 @@
+import readlineSync from 'readline-sync'
+console.log("Hello world, greetings from Shubha")
+const user = readlineSync.question("Who is using this program?")
+console.log("Welcome to my program, "+ user)
