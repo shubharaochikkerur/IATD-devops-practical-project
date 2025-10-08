3dfbdcb575d02d9c3fcbcb73c9ec3a2582d584c9
Shubha Rao-Chikkerur <shubha.raochikkerur3@tafensw.edu.au>
2025-10-08 13:42:08 +1100

    comment added

--- lab1src.js
+++ lab1src.js
@@ -4,3 +4,4 @@ const user = readlineSync.question("Who is using this program?")
 console.log("Welcome to my program, "+ user)
 console.log("another line of code")
 console.log("Adding a line in sample branch")
+//comment is added
